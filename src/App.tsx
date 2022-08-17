20b8db21fc2b9d7d75a846d30a19aa0712ef7af1
Martin Schulze Beckendorf <martin.sb@outlook.de>
2022-08-17 18:21:52 +0200

    Use Bulma Nav Elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
+import { Container, Navbar, Image } from "react-bulma-components";
 import { Link, Outlet } from "react-router-dom";
+
+import logoUrl from './assets/logo.svg';
 
 function App() {
 
   return (
-    <div>
-      {/* TODO Global Navbar */}
-      <nav>
-        <Link to="">Photovoltaik</Link>
-        <Link to="wind">Windkraft</Link>
-        <Link to="biomass">Biomasse</Link>
-        <Link to="water">Wasserkraft</Link>
-      </nav>
+    <Container>
+      {/* Global Navbar */}
+      <Navbar>
+        <Navbar.Brand>
+          <Navbar.Item to="" renderAs={Link}>
+            <Image src={logoUrl} size={32}></Image>
+          </Navbar.Item>
+        </Navbar.Brand>
+        <Navbar.Item to="" renderAs={Link}>Photovoltaik</Navbar.Item>
+        <Navbar.Item to="wind" renderAs={Link}>Windkraft</Navbar.Item>
+        <Navbar.Item to="biomass" renderAs={Link}>Biomasse</Navbar.Item>
+        <Navbar.Item to="water" renderAs={Link}>Wasserkraft</Navbar.Item>
+      </Navbar>
       
       {/* Content, wird gesetzt durch React Router nested Component*/}
       <Outlet />
 
       {/* TODO Global Footer */}
-    </div> 
+    </Container> 
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes,  Route} from 'react-router-dom';
-import PagePv from './components/PagePv';
 
 import App from './App'
-//import './index.css'
-import 'bulma/css/bulma.min.css';
+import PagePv from './components/PagePv';
 import PageWind from './components/PageWind';
 import PageBiomass from './components/PageBiomass';
 import PageWater from './components/PageWater';
+
+//import './index.css'
+import 'bulma/css/bulma.min.css';
 
 const baseUrl = import.meta.env.BASE_URL as string;
 
