539744847e6aa69e179c6dc70174c65c01a7159b
Martin Schulze Beckendorf <martin.sb@outlook.de>
2022-08-17 18:00:30 +0200

    Test root

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Link, Outlet } from "react-router-dom";
 
 function App() {
-  const baseUrl = import.meta.env.BASE_URL as string;
 
   return (
     <div>
--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}>
+        <Route path="" element={<App />}>
           <Route index element={<PagePv />} />
           <Route path="wind" element={<PageWind />} />
           <Route path="biomass" element={<PageBiomass />} />
