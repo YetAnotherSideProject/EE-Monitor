bd50b92ecdaba530776d918b19adeb93385978c2
Martin Schulze Beckendorf <martin.sb@outlook.de>
2022-08-17 18:09:23 +0200

    Test dynamic base url

--- src/main.tsx
+++ src/main.tsx
@@ -10,11 +10,13 @@ import PageWind from './components/PageWind';
 import PageBiomass from './components/PageBiomass';
 import PageWater from './components/PageWater';
 
+const baseUrl = import.meta.env.BASE_URL as string;
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={baseUrl}>
       <Routes>
-        <Route path="" element={<App />}>
+        <Route path="/" element={<App />}>
           <Route index element={<PagePv />} />
           <Route path="wind" element={<PageWind />} />
           <Route path="biomass" element={<PageBiomass />} />
